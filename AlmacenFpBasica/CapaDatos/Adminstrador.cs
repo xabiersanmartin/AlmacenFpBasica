188732b2b5566aff9b26c1b7e6298b75660ba444
Xabier San Martín Oneka <xabottol@hotmail.com>
2019-10-29 09:43:03 +0100

    Clase Administrador hecha.

--- AlmacenFpBasica/CapaDatos/Adminstrador.cs
+++ AlmacenFpBasica/CapaDatos/Adminstrador.cs
@@ -8,5 +8,20 @@ namespace CapaDatos
 {
     class Adminstrador
     {
+        private string nombre { get; set; }
+        private string password { get; set; }
+
+
+        public Adminstrador(string nombre, string password)
+        {
+            this.nombre = nombre;
+            this.password = password;
+        }
+
+        public Adminstrador()
+        {
+        }
+
+
     }
 }
