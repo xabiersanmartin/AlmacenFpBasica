6133a3c683455a98eff35a5f1e58738182a86a6d
Aritz <Aritz@DESKTOP-14ALJML>
2019-10-29 08:46:56 +0100

    Clase Categoria

--- AlmacenFpBasica/CapaDatos/Categoria.cs
+++ AlmacenFpBasica/CapaDatos/Categoria.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    public class Categoria
+    public class Categoria : IEquatable<Categoria>
     {
+        private int codCategoria { get; set; }
+        private string nombre { get; set; }
+
+        public Categoria(int codCategoria, string nombre)
+        {
+            this.codCategoria = codCategoria;
+            this.nombre = nombre;
+        }
+
+        public Categoria()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Categoria);
+        }
+
+        public bool Equals(Categoria other)
+        {
+            return other != null &&
+                   codCategoria == other.codCategoria;
+        }
+
+        public override int GetHashCode()
+        {
+            return -540866772 + codCategoria.GetHashCode();
+        }
     }
 }
