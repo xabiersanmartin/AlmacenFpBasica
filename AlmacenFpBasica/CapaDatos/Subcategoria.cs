8f577ef68c9f0cfe27fe6253d8232f0740cd4eca
Aritz <Aritz@DESKTOP-14ALJML>
2019-10-29 08:53:44 +0100

    Clase subcategoria v2

--- AlmacenFpBasica/CapaDatos/Subcategoria.cs
+++ AlmacenFpBasica/CapaDatos/Subcategoria.cs
@@ -6,9 +6,35 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-   public class Subcategoria
+   public class Subcategoria : IEquatable<Subcategoria>
     {
         private int codSubCategoria { get; set; }
         private string nombreSubCategoria { get; set; }
+
+        public Subcategoria(int codSubCategoria, string nombreSubCategoria)
+        {
+            this.codSubCategoria = codSubCategoria;
+            this.nombreSubCategoria = nombreSubCategoria;
+        }
+
+        public Subcategoria()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Subcategoria);
+        }
+
+        public bool Equals(Subcategoria other)
+        {
+            return other != null &&
+                   codSubCategoria == other.codSubCategoria;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1681949756 + codSubCategoria.GetHashCode();
+        }
     }
 }
