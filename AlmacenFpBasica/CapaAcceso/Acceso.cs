758ef82698404813b28f4fd284240333c28c1e21
Aritz <Aritz@DESKTOP-14ALJML>
2019-10-30 11:39:17 +0100

    Funcion generar base de datos en el caso de que no exista.

--- AlmacenFpBasica/CapaAcceso/Acceso.cs
+++ AlmacenFpBasica/CapaAcceso/Acceso.cs
@@ -4,47 +4,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.IO;
+
 namespace CapaAcceso
 {
     public class Acceso
     {
         private const string DBName = "almacen.bd";
-        private const string SQLScript = @"..\CapaPresentacion\bin\debug\almacen.bd";
+        private const string SQLScript = @"..\script.sql";
         private static bool IsDbRecentlyCreated = false;
-       
-        
-        #region Funciones para el administrador
-        //TODO CargarAdministrador
-        //TODO ModificarPasswordAdministrador
-        #endregion
 
-        #region Funciones para la empresa
-        //TODO CargarEmpresa
-        //TODO ModificarEmpresa
-        #endregion
+        public static void Up()
+        {
+            // Crea la base de datos y registra usuario solo una vez
+            if (!File.Exists(Path.GetFullPath(DBName)))
+            {
+                SQLiteConnection.CreateFile(DBName);
+                IsDbRecentlyCreated = true;
+            }
 
-        #region Funciones para las categorias
-        //TODO CargarCategorias
-        //TODO AñadirCategoria (Admin)
-        //TODO ModificarCategoria (Admin)
-        //TODO EliminarCategoria (Admin)
-        #endregion
+            using (var ctx = GetInstance())
+            {
+                if (IsDbRecentlyCreated)
+                {
+                    using (var reader = new StreamReader(Path.GetFullPath(SQLScript)))
+                    {
+                        var query = "";
+                        var line = "";
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            query += line;
+                        }
 
-        #region Funciones para las subcategorias
-        //TODO CargarSubCategorias
-        //TODO AñadirSubCategoria (Admin)
-        //TODO ModificarSubCategoria (Admin)
-        //TODO EliminarSubCategoria (Admin)
-        #endregion
+                        using (var command = new SQLiteCommand(query, ctx))
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    }
+                }
+            }
+        public static SQLiteConnection GetInstance()
+        {
+            var db = new SQLiteConnection(
+                string.Format("Data Source={0};Version=3;", DBName)
+            );
 
-        #region Funciones para los productos
-        //TODO CargarProductos
-        //TODO ModificarProducto
-        //TODO AñadirProducto (Admin)
-        //TODO EliminarProducto (Admin)
-        #endregion
+            db.Open();
 
-
-      
+            return db;
+        }
+    }   
     }
+
+    #region Funciones para el administrador
+    //TODO CargarAdministrador
+    //TODO ModificarPasswordAdministrador
+    #endregion
+
+    #region Funciones para la empresa
+    //TODO CargarEmpresa
+    //TODO ModificarEmpresa
+    #endregion
+
+    #region Funciones para las categorias
+    //TODO CargarCategorias
+    //TODO AñadirCategoria (Admin)
+    //TODO ModificarCategoria (Admin)
+    //TODO EliminarCategoria (Admin)
+    #endregion
+
+    #region Funciones para las subcategorias
+    //TODO CargarSubCategorias
+    //TODO AñadirSubCategoria (Admin)
+    //TODO ModificarSubCategoria (Admin)
+    //TODO EliminarSubCategoria (Admin)
+    #endregion
+
+    #region Funciones para los productos
+    //TODO CargarProductos
+    //TODO ModificarProducto
+    //TODO AñadirProducto (Admin)
+    //TODO EliminarProducto (Admin)
+    #endregion
+
+
+
+}
 }
