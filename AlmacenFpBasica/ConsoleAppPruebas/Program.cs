b186e4504d9251046b38c3fa6dc4ea8628c3ff67
AlumnoDAM1 <h1213579@mvrht.com>
2019-10-30 11:36:40 +0100

    Pruebas en consola y script del sql

---
+++ AlmacenFpBasica/ConsoleAppPruebas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       string DBName = "almacen.bd";
+         string SQLScript = @"..\CapaPresentacion\bin\debug\almacen.bd";
+         bool IsDbRecentlyCreated = false;
+
+    }
+    }
+}
