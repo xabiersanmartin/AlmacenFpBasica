af77439f52c61aa732baeb0d50cd06702b00cffa
AlumnoDAM1 <h1213579@mvrht.com>
2019-10-30 11:16:44 +0100

    TODOs CapaAcceso

--- AlmacenFpBasica/CapaAcceso/Acceso.cs
+++ AlmacenFpBasica/CapaAcceso/Acceso.cs
@@ -8,5 +8,38 @@ namespace CapaAcceso
 {
     public class Acceso
     {
+        #region Funciones para el administrador
+        //TODO CargarAdministrador
+        //TODO ModificarPasswordAdministrador
+        #endregion
+
+        #region Funciones para la empresa
+        //TODO CargarEmpresa
+        //TODO ModificarEmpresa
+        #endregion
+
+        #region Funciones para las categorias
+        //TODO CargarCategorias
+        //TODO AñadirCategoria (Admin)
+        //TODO ModificarCategoria (Admin)
+        //TODO EliminarCategoria (Admin)
+        #endregion
+
+        #region Funciones para las subcategorias
+        //TODO CargarSubCategorias
+        //TODO AñadirSubCategoria (Admin)
+        //TODO ModificarSubCategoria (Admin)
+        //TODO EliminarSubCategoria (Admin)
+        #endregion
+
+        #region Funciones para los productos
+        //TODO CargarProductos
+        //TODO ModificarProducto
+        //TODO AñadirProducto (Admin)
+        //TODO EliminarProducto (Admin)
+        #endregion
+
+
+
     }
 }
