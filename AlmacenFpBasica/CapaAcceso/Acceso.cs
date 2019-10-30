6c889befb0fb05f983d19ecfee4613e40675e7c7
AlumnoDAM1 <h1213579@mvrht.com>
2019-10-30 11:52:31 +0100

    Comentarios varios

--- AlmacenFpBasica/CapaAcceso/Acceso.cs
+++ AlmacenFpBasica/CapaAcceso/Acceso.cs
@@ -10,13 +10,17 @@ namespace CapaAcceso
 {
     public class Acceso
     {
+        #region Propiedades para la conexion de la base de datos
         private const string DBName = "almacen.bd";
         private const string SQLScript = @"..\script.sql";
         private static bool IsDbRecentlyCreated = false;
+        #endregion
 
+        /// <summary>
+        ///Metodo que crea la base de datos si no existe
+        /// </summary>
         public static void Up()
         {
-            // Crea la base de datos y registra usuario solo una vez
             if (!File.Exists(Path.GetFullPath(DBName)))
             {
                 SQLiteConnection.CreateFile(DBName);
@@ -44,6 +48,11 @@ namespace CapaAcceso
                     }
                 }
             }
+
+        /// <summary>
+        /// Metodo para abrir la conexion de la base de datos
+        /// </summary>
+        /// <returns></returns>
         public static SQLiteConnection GetInstance()
         {
             var db = new SQLiteConnection(
@@ -54,38 +63,38 @@ namespace CapaAcceso
 
             return db;
         }
-    }   
 
+        #region Funciones para el administrador
+        //TODO CargarAdministrador
+        //TODO ModificarPasswordAdministrador
+        #endregion
 
-    #region Funciones para el administrador
-    //TODO CargarAdministrador
-    //TODO ModificarPasswordAdministrador
-    #endregion
+        #region Funciones para la empresa
+        //TODO CargarEmpresa
+        //TODO ModificarEmpresa
+        #endregion
 
-    #region Funciones para la empresa
-    //TODO CargarEmpresa
-    //TODO ModificarEmpresa
-    #endregion
+        #region Funciones para las categorias
+        //TODO CargarCategorias
+        //TODO AñadirCategoria (Admin)
+        //TODO ModificarCategoria (Admin)
+        //TODO EliminarCategoria (Admin)
+        #endregion
 
-    #region Funciones para las categorias
-    //TODO CargarCategorias
-    //TODO AñadirCategoria (Admin)
-    //TODO ModificarCategoria (Admin)
-    //TODO EliminarCategoria (Admin)
-    #endregion
+        #region Funciones para las subcategorias
+        //TODO CargarSubCategorias
+        //TODO AñadirSubCategoria (Admin)
+        //TODO ModificarSubCategoria (Admin)
+        //TODO EliminarSubCategoria (Admin)
+        #endregion
 
-    #region Funciones para las subcategorias
-    //TODO CargarSubCategorias
-    //TODO AñadirSubCategoria (Admin)
-    //TODO ModificarSubCategoria (Admin)
-    //TODO EliminarSubCategoria (Admin)
-    #endregion
+        #region Funciones para los productos
+        //TODO CargarProductos
+        //TODO ModificarProducto
+        //TODO AñadirProducto (Admin)
+        //TODO EliminarProducto (Admin)
+        #endregion
 
-    #region Funciones para los productos
-    //TODO CargarProductos
-    //TODO ModificarProducto
-    //TODO AñadirProducto (Admin)
-    //TODO EliminarProducto (Admin)
-    #endregion
-       
+    }
+
 }
