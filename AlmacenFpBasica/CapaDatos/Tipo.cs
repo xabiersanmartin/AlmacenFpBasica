2a3324f4e32519cf93eb7dc33c854588d0dc49fe
AlumnoDAM1 <h1213579@mvrht.com>
2019-11-05 09:14:52 +0100

    Clase Tipo

---
+++ AlmacenFpBasica/CapaDatos/Tipo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Tipo
+    {
+        public enum TiposProducto { solido, liquido, ropa }
+        public TiposProducto TipoProducto { get; set; }
+
+        public Tipo(TiposProducto tipoProducto)
+        {
+            TipoProducto = tipoProducto;
+        }
+    }
+}
